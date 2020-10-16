90be26ee6a5f403dd357baab636bac7186845316
Wiberg <klutten99@gmail.com>
2020-10-16 23:44:00 +0200

    Added functionality for server class and added ClientHandler.py for handling all the clients

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from hashlib import md5
-
 from Account import Account
+
+import Server
 
 matt = Account("Mattias", "klutten99", md5(b"mattias123"))
 osk = Account("Oskar", "MrW", md5(b"oskar123"))
-print(matt.password == md5(b"mattias123"))
+
+server = Server()
+
