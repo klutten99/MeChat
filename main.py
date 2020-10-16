15769f829bcc8ed7118c462e0b83976f4a7049f7
Wiberg <klutten99@gmail.com>
2020-10-17 00:41:49 +0200

    Revert "Added functionality for server class and added ClientHandler.py for handling all the clients"
    This reverts commit 90be26ee

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from hashlib import md5
-from Account import Account
 
-import Server
+from Account import Account
 
 matt = Account("Mattias", "klutten99", md5(b"mattias123"))
 osk = Account("Oskar", "MrW", md5(b"oskar123"))
-
-server = Server()
-
+print(matt.password == md5(b"mattias123"))
