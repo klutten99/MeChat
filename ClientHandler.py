3d3c88bdae2a33c1d7d146b63f3a133fc82eadf9
Wiberg <klutten99@gmail.com>
2020-10-17 00:41:26 +0200

    Test

--- ClientHandler.py
+++ ClientHandler.py
@@ -1,12 +1,25 @@
-import threading
+import _thread
 import Server
 
 
-class ClientHandler(threading.Thread):
-    def __init__(self, connection, address, server):
+class ClientHandler:
+    def run(self):
+        while 1:
+            try:
+                data = self.connection.recv(self.server.buffer_size)
+                if not data: break;
+                print(data.decode("utf-8"))
+                self.connection.send(b'Server: ' + data)
+            except ConnectionResetError:
+                self.connection.close()
+                print(str(address) + " has lost connection.")
+                return
+        self.connection.close()
+        print(str(address) + " has disconnected.")
+
+    def __init__(self, connection, address):
         self.server = server
         self.connection = connection
         self.address = address
-
-    def run(self):
+        _thread.start_new_thread(run, ())
 
--- Server.py
+++ Server.py
@@ -1,32 +1,30 @@
 import socket
-import threading
-
 import ClientHandler
+import _thread
 
-class Server(threading.Thread):
+
+class Server:
     clients = []
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def __init__(self, port=1337, host="127.0.0.1", buffer_size=1024, max_connections=3):
-        threading.Thread.__init__(self)
         self.port = port
         self.host = host
         self.buffer_size = buffer_size
         self.max_connections = max_connections
 
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind((host, port))
         self.sock.listen()
         print("Server initiated!")
-        print("Starting server thread..")
-        self.start()
 
     def run(self):
         print("Started server thread!")
         print("Accepting incoming connections...")
+
         while 1:
             connection, address = self.sock.accept()
             if len(self.clients) < self.max_connections:
-                client = ClientHandler(connection, address, self)
+                client = ClientHandler(connection, address)
                 self.clients.append(client)
                 print(str(address) + " connected to the server.")
             else:
@@ -37,5 +35,7 @@ class Server(threading.Thread):
                 connection.close()
                 print("Rejected client trying to connect. Max connections reached!")
 
+    def start(self):
+        print("Starting server thread..")
+        _thread.start_new_thread(run, ())
 
-server = Server()
